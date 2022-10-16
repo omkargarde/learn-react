ffabc416bac1274470d98921ee3b522d0571c668
omkargarde <omyagarde@gmail.com>
2022-10-16 19:32:23 +0530

    create expense component , iterated json data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import ExpenseItems from "./components/ExpenseItems";
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  const items = Object.keys(expenses).map((key, index) => (
+    <div key={index}>
+      <ExpenseItems
+        title={expenses[key].title}
+        amount={expenses[key].amount}
+        date={expenses[key].date}
+      ></ExpenseItems>
+    </div>
+  ));
   return (
     <div>
       <h2>Let's get started!</h2>
+      {items}
     </div>
   );
 }
